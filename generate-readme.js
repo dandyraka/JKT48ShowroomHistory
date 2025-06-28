37f599e63431c5b36a7249218a008ab341cadb51
Dandy Raka <dandyraka13@gmail.com>
2025-06-29 01:38:02 +0700

    Update generate-readme.js

--- generate-readme.js
+++ generate-readme.js
@@ -2,9 +2,9 @@ import fs from "fs";
 
 const history = fs.readFileSync("history.txt", "utf-8").trim().split("\n");
 
-const tableRows = history.map(line => {
+const tableRows = history.map((line, index) => {
     const [member, tanggal] = line.split(" | ");
-    return `| ${member} | ${tanggal} |`;
+    return `| ${index + 1} | ${member} | ${tanggal} |`;
 });
 
 const markdownTable = `# History Live Showroom JKT48
